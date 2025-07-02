fa51303467ed00116f8af4518610b805222d06a8
Astar18 <aleister200213@gmail.com>
2025-07-02 12:39:54 -0500

    url front

--- src/assets/environment/environment.ts
+++ src/assets/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:5190/api',
-  apiUrl: 'https://dilipa.ddns.net/backend/api',
+  apiUrl: 'https://dilipa.ddns.me/backend/api',
 
 };
